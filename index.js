bbce940193a5d130f545ffdf560f53617690137d
Franziska Chen <chenfranziska@gmail.com>
2024-05-11 16:09:46 +0200

    Write code

---
+++ index.js
@@ -0,0 +1,31 @@
+// # IMPORTED MODULES AND LIBRARIES:
+// fs and https are build-in modules for eg downloading images and save them into a specific directory
+import fs from 'node:fs';
+import client from 'node:https';
+// library to get html text from a https URL
+import fetch from 'node-fetch';
+
+// # ACTUAL CODE:
+// fetch plain text or HTML
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+
+// function: download image (url) and save it to a specific directory (filepath)
+function downloadImage(url, filepath) {
+  client.get(url, (res) => {
+    res.pipe(fs.createWriteStream(filepath));
+  });
+}
+
+// extract all image URLs from the html text but I used it without any variables only with .match - it is just very confusing so I will put this documentation here:
+// code with extra variable:  const regEx = /(?<= src=").+?jpg/g;
+// code with extra variable: const finalImage = [...body.match(regEx)];
+
+// Loop to download first ten images and rename them
+for (let a = 0; a < 10; a++) {
+  const number = a + 1;
+  const rename = `${number.toString().padStart(2, '0')}.jpg`; // padStart() helps to pad a number with leading zeros
+  downloadImage(body.match(/(?<= src=").+?jpg/g)[a], `memes/${rename}`);
+}
